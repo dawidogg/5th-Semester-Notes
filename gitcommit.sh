a385146118a2b34fc75f4b2e0706202bdd991949
Denis Davidoglu <dawidogg.feed@gmail.com>
2023-10-20 10:34:49 +0300

    Update notes

---
+++ gitcommit.sh
@@ -0,0 +1,8 @@
+git add "./index.org"
+git add "./Computer Communications/cc_notes.org"
+git add "./Computer Communications/docs/Computer Networking. A Top Down Approach, 7th.org"
+git add "./ATA I/docs/Eric Zürcher-Book (English).org"
+git add "./ATA I/at1_notes.org"
+git add ".github"
+git add "gitcommit.sh"
+git commit -m "Update notes"
